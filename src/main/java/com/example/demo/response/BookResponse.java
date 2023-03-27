d2e41c0ce981abaa55b609b0eb32c1182f301a73
Zatvorn1k <tiberhinte@gmail.com>
2023-03-28 00:09:25 +0300

    Last

--- src/main/java/com/example/demo/response/BookResponse.java
+++ src/main/java/com/example/demo/response/BookResponse.java
@@ -7,13 +7,13 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class BookResponse extends BaseResponse {
-    public BookResponse(boolean success, String message, BookEntity publisher){
+    public BookResponse(boolean success, String message, BookEntity book){
         super(success, message);
-        this.publisher = publisher;
+        this.book = book;
     }
     public BookResponse(BookEntity publisher) {
         super(true, "Publishing data");
     }
-    private BookEntity publisher;
+    private BookEntity book;
 }
 
